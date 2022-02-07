8b2f8ba84e71f6be5f32686ddf1b20dc871a68f1
Chris <github@mrz.angrydome.com>
2022-02-07 10:41:53 +0000

    fix for 3.7

--- scripts/aws_cron.sh
+++ scripts/aws_cron.sh
@@ -2,11 +2,12 @@
 
 # This is a script meant to run a full sync
 
-declare -x BASE_PATH="~ec2-user/snyk-sync/"
+declare -x HOMEDIR="/home/ec2-user/"
+declare -x BASE_PATH="${HOMEDIR}snyk-sync/"
 declare -x PYTHON="${BASE_PATH}.venv/bin/python"
 declare -x SNYK_SYNC="${BASE_PATH}snyk_sync/cli.py"
 
-declare -x SYNC_WORKING_DIR="~ec2-user/config/"
+declare -x SYNC_WORKING_DIR="${HOMEDIR}config/"
 
 eval "$($PYTHON ${BASE_PATH}scripts/export_aws_secrets.py)"
 
@@ -17,3 +18,4 @@ else
     cd $SYNC_WORKING_DIR || exit
     git fetch --depth 1 origin
 fi
+
